8de03370d48d5fd2120d1ddabc64b93454eb2bf8
Ferrel John Fernando <f.syntaxer@gmail.com>
2023-11-29 22:47:01 +0700

    feat: add product categories repository

---
+++ src/modules/products/categories/repository.ts
@@ -0,0 +1,24 @@
+import { eq } from 'drizzle-orm';
+import db from '../../../db';
+import {
+  newProductCategory,
+  productCategories,
+  productCategory,
+} from '../../../db/schema/product-category';
+
+export class ProductCategoryRepository {
+  getProductCategories = async () => db.select().from(productCategories);
+  getProductCategoryById = async (id: number) =>
+    db.select().from(productCategories).where(eq(productCategories.id, id));
+  getProductCategoryByName = async (name: string) =>
+    db.select().from(productCategories).where(eq(productCategories.name, name));
+  createProductCategory = async (productCategory: newProductCategory) =>
+    db
+      .insert(productCategories)
+      .values(productCategory)
+      .returning({ id: productCategories.id, name: productCategories.name });
+  updateProductCategory = async (id: number, updateProductCategory: productCategory) =>
+    db.update(productCategories).set(updateProductCategory).where(eq(productCategories.id, id));
+  softDeleteProductCategory = async (id: number) =>
+    db.update(productCategories).set({ isActive: false }).where(eq(productCategories.id, id));
+}
