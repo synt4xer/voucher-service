20ed1763539a5e3e8442e39715d763469d5fc766
Ferrel John Fernando <f.syntaxer@gmail.com>
2023-12-02 17:53:54 +0700

    refactor: Add redis for improve authentication workflow

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,14 +1,18 @@
 import _ from 'lodash';
+import { logger } from '../utils/logger';
+import redisUtil from '../utils/redis.util';
 import { RequestWithUser } from '../interfaces';
-import { verifyToken } from '../utils/jwt.utils';
+import { verifyToken } from '../utils/jwt.util';
 import { NextFunction, Response } from 'express';
+import { AppConstant } from '../utils/app-constant';
 import {
   AuthTokenMissingException,
   WrongAuthTokenException,
 } from '../exceptions/unauthorized.exception';
+
 import { AuthenticationRepository } from '../modules/authentication/authentication.repository';
 
-const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+const authMiddleware = async (req: RequestWithUser, _res: Response, next: NextFunction) => {
   const userRepository: AuthenticationRepository = new AuthenticationRepository();
   const authorization = req.header('Authorization');
 
@@ -16,8 +20,16 @@ const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFun
     next(new AuthTokenMissingException());
   }
 
+  const authToken = authorization!.replace('Bearer ', '');
+
   try {
-    const verify = verifyToken(authorization!.replace('Bearer ', ''));
+    const isExist = await redisUtil.isExists(`${AppConstant.REDIS_AUTH_KEY}${authToken}`);
+
+    if (!isExist) {
+      next(new WrongAuthTokenException());
+    }
+
+    const verify = verifyToken(authToken);
     const _uuid = verify._uuid;
     const user = await userRepository.getUserByUuid(_uuid);
 
@@ -29,6 +41,7 @@ const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFun
     req.user = { uuid, name, email, phone };
     next();
   } catch (error) {
+    logger.error('authMiddleware.error', error);
     next(new WrongAuthTokenException());
   }
 };
