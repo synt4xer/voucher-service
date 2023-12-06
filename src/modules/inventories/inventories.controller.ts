d0c50bff827e0ef07946ad647c7bd05d0f597e3f
Ferrel John Fernando <f.syntaxer@gmail.com>
2023-12-06 20:15:55 +0700

    refactor: Rename inventories file and some changes

--- src/modules/inventories/inventories.controller.ts
+++ src/modules/inventories/inventories.controller.ts
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import { logger } from '../../utils/logger';
 import { APIResponse } from '../../interfaces';
-import { InventoryService } from './inventories/service';
+import { InventoryService } from './inventories.service';
 import { NextFunction, Request, Response } from 'express';
 
 class InventoryController {
@@ -34,12 +34,11 @@ class InventoryController {
   };
   addStock = async (req: Request, res: Response<APIResponse>, next: NextFunction) => {
     try {
-        const productId = _.get(req, 'params.productId');
-        const body = _.get(req, 'body');
-        
-        const data = await this.service.updateStock({ operations: 'plus', ...body }, +productId);
-        res.status(200).json({ success: true, data });
-        
+      const productId = _.get(req, 'params.productId');
+      const body = _.get(req, 'body');
+
+      const data = await this.service.updateStock({ operations: 'plus', ...body }, +productId);
+      res.status(200).json({ success: true, data });
     } catch (error) {
       logger.error('inventory.addStock.error', error);
       next(error);
@@ -47,12 +46,11 @@ class InventoryController {
   };
   reduceStock = async (req: Request, res: Response<APIResponse>, next: NextFunction) => {
     try {
-        const productId = _.get(req, 'params.productId');
-        const body = _.get(req, 'body');
-        
-        const data = await this.service.updateStock({ operations: 'min', ...body }, +productId);
-        res.status(200).json({ success: true, data });
-        
+      const productId = _.get(req, 'params.productId');
+      const body = _.get(req, 'body');
+
+      const data = await this.service.updateStock({ operations: 'min', ...body }, +productId);
+      res.status(200).json({ success: true, data });
     } catch (error) {
       logger.error('inventory.reduceStock.error', error);
       next(error);
---
+++ src/modules/inventories/inventories.service.ts
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+import { NewInventory, Inventory } from '../../db/schema/inventory';
+import { ProductInventoryNotFoundException } from '../../exceptions/bad-request.exception';
+import { InventoryRepository } from './inventories.repository';
+import { RequestUpdateInventory } from '../../interfaces';
+
+export class InventoryService {
+  private readonly repository: InventoryRepository;
+
+  constructor() {
+    this.repository = new InventoryRepository();
+  }
+
+  // * read
+  getAll = async () => this.repository.getInventories();
+  // * get one
+  getOneByProductId = async (productId: number) =>
+    this.repository.getInventoryByProductId(productId);
+  // * update
+  updateStock = async (payload: RequestUpdateInventory, productId: number) => {
+    try {
+      const [existingInventory] = await this.repository.getInventoryByProductId(productId);
+
+      if (_.isEmpty(existingInventory)) {
+        throw new ProductInventoryNotFoundException(productId);
+      }
+
+      if (payload.operations === 'plus') {
+        return this.repository.addStock(productId, payload, existingInventory);
+      } else if (payload.operations === 'min') {
+        return this.repository.reduceStock(productId, payload, existingInventory);
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+}
