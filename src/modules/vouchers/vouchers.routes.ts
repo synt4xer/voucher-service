dbf82a449f64c99db80143ab38a13cda57fc2a64
Ferrel John Fernando <38653000+synt4xer@users.noreply.github.com>
2023-12-11 15:02:43 +0700

    refactor: Authentication middleware and add it to voucher API (#12)
    - Refactor Authentication middleware
    - Add Authentication middleware to Voucher API

--- src/modules/vouchers/vouchers.routes.ts
+++ src/modules/vouchers/vouchers.routes.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import VoucherController from './vouchers.controller';
+import authMiddleware from '../../middlewares/auth.middleware';
 
 const router = express.Router();
 const voucherController = new VoucherController();
 
-router.get('/', voucherController.getAll);
-router.post('/', voucherController.create);
-router.get('/:code', voucherController.getOne);
-router.patch('/:code', voucherController.update);
-router.delete('/:code', voucherController.delete);
+router.get('/', authMiddleware, voucherController.getAll);
+router.post('/', authMiddleware, voucherController.create);
+router.get('/:code', authMiddleware, voucherController.getOne);
+router.patch('/:code', authMiddleware, voucherController.update);
+router.delete('/:code', authMiddleware, voucherController.delete);
 
 export default router;
