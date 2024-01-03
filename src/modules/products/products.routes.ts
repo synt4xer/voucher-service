aae2cae09d32787e8b00d10b8ee377ffa5105b45
Ferrel John Fernando <f.syntaxer@gmail.com>
2024-01-03 14:49:26 +0700

    fix: product router

--- src/modules/products/products.routes.ts
+++ src/modules/products/products.routes.ts
@@ -15,11 +15,11 @@ router.patch('/categories/:id', authMiddleware, categoryController.update);
 router.delete('/categories/:id', authMiddleware, categoryController.delete);
 
 // * products
+router.get('/list', authMiddleware, productController.list);
 router.get('/', authMiddleware, productController.getAll);
 router.post('/', authMiddleware, productController.create);
 router.get('/:id', authMiddleware, productController.getOne);
 router.patch('/:id', authMiddleware, productController.update);
 router.delete('/:id', authMiddleware, productController.delete);
-router.get('/list', authMiddleware, productController.list);
 
 export default router;
